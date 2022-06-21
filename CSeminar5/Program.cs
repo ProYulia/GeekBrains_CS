5faa369d6d963408025b3340f2a6232a4386feb4
ProYulia <106432235+ProYulia@users.noreply.github.com>
2022-06-21 12:02:56 +0300

    hw published

--- CSeminar5/Program.cs
+++ CSeminar5/Program.cs
@@ -10,7 +10,7 @@ int[] CreateRandomArray(int size, int min, int max)
         int[] newArray = new int[size];
         for (int i = 0; i < size; i++)
         {
-        newArray[i] = new Random().Next(min, max + 1);
+        newArray[i] = new Random().Next(min, max);
         Console.Write(newArray[i] + " ");
         }
     Console.WriteLine();
@@ -42,7 +42,7 @@ int[] CreateRandomArray(int size, int min, int max)
         int[] newArray = new int[size];
         for (int i = 0; i < size; i++)
         {
-        newArray[i] = new Random().Next(min, max + 1);
+        newArray[i] = new Random().Next(min, max);
         Console.Write(newArray[i] + " ");
         }
     Console.WriteLine();
